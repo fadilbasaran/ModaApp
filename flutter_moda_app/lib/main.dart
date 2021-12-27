a9e44ecdc171edc8fb8983de578ca67bdb464081
fadilbasaran <59345355+fadilbasaran@users.noreply.github.com>
2021-12-28 01:55:49 +0300

    nextApp

--- flutter_moda_app/lib/main.dart
+++ flutter_moda_app/lib/main.dart
@@ -29,11 +29,11 @@ class _AnaSayfaState extends State<AnaSayfa> {
         backgroundColor: Colors.transparent,
         elevation: 0,
         title: const Text(
-          'Moda Uygulaması',
+          'ModaApp',
           style: TextStyle(
               fontFamily: 'Montserrat',
-              color: Colors.black,
-              fontSize: 22,
+              color: Colors.black87,
+              fontSize: 25,
               fontWeight: FontWeight.bold),
         ),
         actions: [
@@ -47,18 +47,110 @@ class _AnaSayfaState extends State<AnaSayfa> {
       body: ListView(
         padding: const EdgeInsets.only(top: 15),
         children: [
-          Container(
-            color: Colors.blue,
-            height: 150,
+          //Üst taraftali profil listesi
+          SizedBox(
+            //color: Colors.blue,
+            height: 140,
             width: double.infinity,
             child: ListView(
               padding: const EdgeInsets.all(10),
               scrollDirection: Axis.horizontal,
               children: [
                 listeElemani('assets/model1.jpeg', 'assets/chanellogo.jpg'),
+                const SizedBox(width: 25),
+                listeElemani('assets/model2.jpeg', 'assets/louisvuitton.jpg'),
+                const SizedBox(width: 25),
+                listeElemani('assets/model3.jpeg', 'assets/chloelogo.png'),
+                const SizedBox(width: 25),
+                listeElemani('assets/model1.jpeg', 'assets/chanellogo.jpg'),
+                const SizedBox(width: 25),
+                listeElemani('assets/model2.jpeg', 'assets/louisvuitton.jpg'),
+                const SizedBox(width: 25),
+                listeElemani('assets/model3.jpeg', 'assets/chloelogo.png'),
+                const SizedBox(width: 25),
+                listeElemani('assets/model1.jpeg', 'assets/chanellogo.jpg'),
+                const SizedBox(width: 25),
+                listeElemani('assets/model2.jpeg', 'assets/louisvuitton.jpg'),
+                const SizedBox(width: 25),
+                listeElemani('assets/model3.jpeg', 'assets/chloelogo.png'),
+                const SizedBox(width: 25),
               ],
             ),
           )
+          //Card
+          ,
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Material(
+              borderRadius: BorderRadius.circular(16),
+              elevation: 4,
+              color: Colors.blue.shade300,
+              child: Container(
+                height: 570,
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(35),
+                            image: const DecorationImage(
+                                image: AssetImage('assets/model1.jpeg'),
+                                fit: BoxFit.cover),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width - 160,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                'Daisy',
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                '34 min ago',
+                                style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Icon(
+                          Icons.more_vert,
+                          color: Colors.grey,
+                          size: 22,
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 15),
+                    const Text(
+                        'This ooficial website features a ribbed knit zipper jacket that  is'
+                        ' modern anda stylish. It looks very temparent and is recommend friends'),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -74,22 +166,44 @@ class _AnaSayfaState extends State<AnaSayfa> {
               height: 75,
               width: 75,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(40),
+                borderRadius: BorderRadius.circular(35),
                 image: DecorationImage(
                     image: AssetImage(imagePath), fit: BoxFit.cover),
               ),
             ),
-            Container(
-              height: 25,
-              width: 35,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                image: DecorationImage(
-                    image: AssetImage(logoPath), fit: BoxFit.cover),
+            Positioned(
+              top: 50,
+              left: 50,
+              child: Container(
+                height: 25,
+                width: 25,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                      image: AssetImage(logoPath), fit: BoxFit.contain),
+                ),
               ),
             )
           ],
-        )
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Container(
+          height: 30,
+          width: 75,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.brown,
+          ),
+          child: const Center(
+            child: Text(
+              'Fallow',
+              style: TextStyle(
+                  fontFamily: 'Montserrat', fontSize: 14, color: Colors.white),
+            ),
+          ),
+        ),
       ],
     );
   }
