46c2107ed2c36ec680823ab8db2677ce5bea9617
fadilbasaran <59345355+fadilbasaran@users.noreply.github.com>
2022-01-01 23:38:42 +0300

    devam

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class ModaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       home: AnaSayfa(),
     );
   }
